c85079e92e31dbcb6f6e43df408051de0b5b628a
David Brown <dbrown@verisk.com>
2018-09-10 11:30:06 -0500

    Added back annotation for logging

--- src/test/groovy/iix/util/DecToolCommandTest.groovy
+++ src/test/groovy/iix/util/DecToolCommandTest.groovy
@@ -1,5 +1,6 @@
 package iix.util
 
+import groovy.util.logging.Slf4j
 import spock.lang.Specification
 import util.toolkit.stringtools.exceptions.ClientErrorException
 import util.toolkit.stringtools.exceptions.FpeDispatcherException
@@ -18,7 +19,7 @@ import java.sql.SQLException
 import java.sql.Statement
 import java.sql.Timestamp
 
-
+@Slf4j
 class DecToolCommandTest extends Specification {
 
     def "Test selecting request_ids as input to the deletion process" () {
@@ -251,46 +252,5 @@ class DecToolCommandTest extends Specification {
         expect:
         ts
     }
-
-    /*def "getConnection test this method for returning a valid jdbc connection string from the ora_messenger.xml file" () {
-        given:
-        File f = new File("src/main/resources/ora_messenger.xml")
-        String sysDate = ";"
-
-        DecToolCommand hwc = new DecToolCommand()
-        OraMessengerBean _omb = hwc.parseOraMessenger(f, "MVR")
-        java.sql.Connection conn = null
-        try {
-//            conn = hwc.getConnection("MVR", "TEST", f)
-            conn =  DBConnection.CreateConnection(_omb.get_javaURL(), _omb.getUser(), _omb.getEncpPswd())
-            sysDate = DBConnection.getDBSysdate(conn)
-        } catch (Exception e) {
-            e.printStackTrace()
-        }
-
-        expect:
-        sysDate.contains("Sysdate")
-    }*/
-
-    /*def "ParseJavaURL test this method for the string extracted from the right side of the '@' sign from JavaURl"() {
-        given:
-        String url = "(DESCRIPTION=(ADDRESS=(PROTOCOL=tcp)(HOST=DEVTIP)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=oratestM.util)))"
-        DecToolCommand hwc = new DecToolCommand()
-        String _url =  hwc.parseJavaURL(url)
-
-        expect:
-        _url.equals("jdbc:oracle:thin:@//DEVTIP:1521/oratestM.util")
-    }*/
-
-    /*def "ParseOraMessenger test this method for the default local file: src/main/resources/ora_messenger.xmls"() {
-        given:
-        File f = new File("src/main/resources/ora_messenger.xml")
-
-        DecToolCommand hwc = new DecToolCommand()
-        String oraMessenger = hwc.parseOraMessenger(f, "MVR")
-
-        expect:
-        oraMessenger.contains("JavaURL")
-    }*/
 
 }
